0c6b4f28bed015f56e216b4dbcacc2b10aa2fa63
Proshkin Mikhail <chernyimisha@rambler.ru>
2022-12-26 14:14:38 +0300

    Выполнено DZ1

--- Task06/Program.cs
+++ Task06/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 6: Напишите программу, которая на вход
+принимает число и выдаёт, является ли число чётным
+(делится ли оно на два без остатка).*/
+
+Console.WriteLine("Введите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0) Console.WriteLine($"Введенноe число {number} четное");
+else Console.WriteLine($"Введенноe число {number} нечетное");
--- Task08/Program.cs
+++ Task08/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 8: Напишите программу, которая на вход
+принимает число (N), а на выходе показывает все чётные
+числа от 1 до N.*/
+
+Console.WriteLine("Введите натуральное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+while (count <= number) //выводит все четные числа до N, не включаяя его самого. Если надо включая, то while (count <= number)
+{
+    Console.Write (count + " ");
+    count = count + 2;
+}
